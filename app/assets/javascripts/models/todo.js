667af7f2e724dbcb1ecb35e59c18619494b69762
Tommaso Villa <tommaso.villa@betvictor.com>
2014-07-31 16:05:51 +0200

    Store and Header implementation

--- app/assets/javascripts/models/todo.js
+++ app/assets/javascripts/models/todo.js
@@ -1,23 +1,23 @@
-// App.Todo = DS.Model.extend({
-//   title: DS.attr('string'),
-//   isCompleted: DS.attr('boolean')
-// });
+App.Pics = DS.Model.extend({
+  title: DS.attr('string'),
+  isCompleted: DS.attr('boolean')
+});
 
 
-// App.Todo.FIXTURES = [
-//  {
-//    id: 1,
-//    title: 'Learn Ember.js',
-//    isCompleted: true
-//  },
-//  {
-//    id: 2,
-//    title: '...',
-//    isCompleted: false
-//  },
-//  {
-//    id: 3,
-//    title: 'Profit!',
-//    isCompleted: false
-//  }
-// ];
+App.Pics.FIXTURES = [
+ {
+   id: 1,
+   title: 'Learn Ember.js',
+   isCompleted: true
+ },
+ {
+   id: 2,
+   title: '...',
+   isCompleted: false
+ },
+ {
+   id: 3,
+   title: 'Profit!',
+   isCompleted: false
+ }
+];
--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,15 +1,16 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 App.Router.map(function() {
-  this.resource('people', function(){
-    this.route('person');
-  });
   this.route('about');
   this.route('skills');
+  this.route('blog');
   this.resource('work', function(){
     this.route('web');
     this.route('front-end');
     this.route('photography');
+  });
+  this.resource('people', function(){
+    this.route('person');
   });
 });
 
