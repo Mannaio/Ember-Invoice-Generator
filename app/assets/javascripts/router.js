784a65da16d6f2b4e22395648f4d89085d4b6b58
Tommaso <tmms.vll@gmail.com>
2014-08-01 12:28:26 +0200

    Routes
    Path structure defined

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -5,12 +5,9 @@ App.Router.map(function() {
   this.route('skills');
   this.route('blog');
   this.resource('work', function(){
+    this.route('photography');
     this.route('web');
     this.route('front-end');
-    this.route('photography');
-  });
-  this.resource('people', function(){
-    this.route('person');
   });
 });
 
