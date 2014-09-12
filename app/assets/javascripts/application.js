1ee7d6c151c056ac957cef00f09939b90dcc59a4
Tommaso Villa <tommaso.villa@betvictor.com>
2014-09-12 16:54:17 +0200

    Foundation rails and Rails Layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,19 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
 //= require jquery
-//= require handlebars
-//= require ember
-//= require ember-data
-//= require_self
-//= require todos
-
-// for more details see: http://emberjs.com/guides/application/
-App = Ember.Application.create();
-
+//= require jquery_ujs
+//= require foundation
+//= require_tree .
+$(function() {
+  $(document).foundation();
+});
