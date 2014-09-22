e0a5bca2d8071d8d7fad8a050ecc9e859398083d
Tommaso Villa <tommaso.villa@betvictor.com>
2014-09-22 11:55:36 +0200

    Remove Routes

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,20 +1,3 @@
 // For more information see: http://emberjs.com/guides/routing/
 
-App.Router.map(function() {
-  this.route('about');
-  this.route('skills');
-  this.route('blog');
-  this.resource('work', function(){
-    this.route('photography');
-    this.route('web');
-    this.route('front-end');
-  });
-});
-
-
-App.AboutRoute = Ember.Route.extend({
-  model: function() {
-    return this.store.find('person');
-  }
-}) 
 
