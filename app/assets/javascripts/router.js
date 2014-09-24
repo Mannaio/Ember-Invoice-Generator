ca828ae7f3283ced582f4c89084f57c47f958faa
Tommaso Villa <tommaso.villa@betvictor.com>
2014-09-24 17:17:11 +0200

    Wiew transactions and created

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,3 +1,23 @@
 // For more information see: http://emberjs.com/guides/routing/
 
+App.Router.map(function(){
+    this.resource('transactions', function(){
+        this.route('create');
+    });
+});
+
+App.IndexRoute = Ember.Route.extend({
+    redirect: function(){
+        this.transitionTo('transactions');
+    }
+});
+
+App.CreateRoute = Ember.Route.extend({
+
+    renderTemplate: function() {
+      this.render('transactions.create');
+    },
+
+});
+
 
