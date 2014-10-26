a49c36c92558ac1c8b1fc719e0dd4667c33757f2
Tommaso Villa <tmms.vll@gmail.com>
2014-10-26 17:16:49 +0100

    Iva percentage done

--- app/assets/javascripts/controllers/transactions_controllers.js
+++ app/assets/javascripts/controllers/transactions_controllers.js
@@ -28,18 +28,27 @@ App.TransactionsController=Ember.ArrayController.extend({
 	    total:null,
 	    selectContentIva:null,
 	    iva:null,
+	    risultato:null,
+	    somma:null,
 	    weight:null
 	 });
     
      aNewThread.reopen({
       calcTotal:function(){
             this.set("total",this.get("quantita")*this.get("selectContentTariffa"));
-          }.observes("quantita","selectContentTariffa"),
+         }.observes("quantita","selectContentTariffa"),
+      percTotal:function(){
+      		this.set("iva",this.get("total")*this.get("selectContentIva"));
+      	 }.observes("total","selectContentIva"),
+      totatResult:function(){
+      		this.set("risultato",this.get("total") + this.get("iva"));
+      	 }.observes("total","iva"),
+   //    sumTotal:function(){
+   //    		var sum = function(s1,s2){ return s1 + s2;}
+   //    		return
+			// this.get("controller").getEach("total").reduce(sum);
+   //    	}.property("controller.@each.total"),
       });      
-      // percTotal:function(){
-      // 		this.set("iva",this.get("total")*this.get("selectContentIva") / 100));
-      // 	 }.observes("total", "selectContentIva"),
-      // });
     
       return aNewThread;
     
--- app/assets/javascripts/views/view.js
+++ app/assets/javascripts/views/view.js
@@ -10,3 +10,27 @@ App.TotalView = Ember.View.extend({
 	}).property('controller.newThread.selectContentTariffa', 'controller.newThread.quantita')
 });
 
+App.IvaView = Ember.View.extend({
+ templateName:"iva",
+ tagName: 'input',
+ attributeBindings: ['iva:value', 'placeholder', 'type'],
+ type: 'number',
+ placeholder: null,
+ iva: (function() {
+    var res= this.get('controller.newThread.iva');
+    return isNaN(res)?"":res;
+	}).property('controller.newThread.selectContentIva', 'controller.newThread.total')
+});
+
+App.RisultatoView = Ember.View.extend({
+ templateName:"risultato",
+ tagName: 'input',
+ attributeBindings: ['risultato:value', 'placeholder', 'type'],
+ type: 'number',
+ placeholder: null,
+ risultato: (function() {
+ 	var res= this.get('controller.newThread.risultato');
+ 	return isNaN(res)?"":res;
+ 	}).property('controller.newThread.total', 'controller.newThread.iva')
+});
+
