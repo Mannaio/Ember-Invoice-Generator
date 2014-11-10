4e543e700cb80b4a17895ecbbe9dace0329d5a35
Tommaso Villa <tmms.vll@gmail.com>
2014-11-10 08:03:40 +0100

    New Layouts

--- app/assets/javascripts/models/todo.js
+++ app/assets/javascripts/models/todo.js
@@ -1,11 +1,13 @@
 App.Fattura = DS.Model.extend({
-  name   : DS.attr('string')
+  name   	: DS.attr('string'),
+  quantita	: DS.attr('string')
 });
 
 
 App.Fattura.FIXTURES = [
  {
    id: 1,
-   name: 'Fattura Num 1'
+   name: 'Fattura Num 1',
+   quantita: '100'
  }
 ];
