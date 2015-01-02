3c95431dcd03d652b104cc24f718ab3edfbafec7
Tommaso Villa <tmms.vll@gmail.com>
2015-01-02 13:35:48 +0000

    Fattura and quantita operation

--- app/assets/javascripts/models/todo.js
+++ app/assets/javascripts/models/todo.js
@@ -11,7 +11,25 @@ App.Transaction = DS.Model.extend({
   fattura       : DS.belongsTo('fattura'),
   tariffa       : DS.attr('string'),
   iva           : DS.attr('string'),
-  isChecked     : DS.attr('boolean')
+  isChecked     : DS.attr('boolean'),
+  updateTotal: function() {
+
+  // get the reference to the values of fare and quantity
+  var quantita = this.get('quantita'),
+      tariffa = this.get('tariffa');
+
+  // massage them to make sure your stuff is not gonna break
+  if (isNaN(tariffa)) { tariffa = 0; }
+  if (isNaN(quantita)) { quantita = 0; }
+
+  // calculate
+  var total = tariffa * quantita;
+
+  // set the total
+  this.set('total', total);
+
+}.observes('quantita', 'tariffa')
+
 });
 
 App.Fattura.reopenClass({
--- app/assets/javascripts/views/view.js
+++ app/assets/javascripts/views/view.js
@@ -1,34 +1,34 @@
-App.TotalView = Ember.View.extend({
- templateName:"total",
- tagName: 'p',
- attributeBindings: ['total:value'],
- placeholder: null,
- total: (function() {
-   var res= parseInt(this.get('controller.quantita')) * parseInt(this.get('controller.tariffa'));
-   return isNaN(res)?"":res;
-   }).property('controller.tariffa', 'controller.quantita')
-});
+// App.TotalView = Ember.View.extend({
+//  templateName:"total",
+//  tagName: 'p',
+//  attributeBindings: ['total:value'],
+//  placeholder: null,
+//  total: (function() {
+//    var res= parseInt(this.get('controller.quantita')) * parseInt(this.get('controller.tariffa'));
+//    return isNaN(res)?"":res;
+//    }).property('controller.tariffa', 'controller.quantita')
+// });
 
-App.IvamountView = Ember.View.extend({
- templateName:"ivamount",
- tagName: 'p',
- attributeBindings: ['iva:value', 'placeholder', 'type'],
- type: 'number',
- placeholder: null,
- iva: (function() {
-  var res= parseInt(this.get('controller.iva')) * parseInt(this.get('total'));
-  return isNaN(res)?"":res;
-	}).property('controller.iva', 'total')
-});
+// App.IvamountView = Ember.View.extend({
+//  templateName:"ivamount",
+//  tagName: 'p',
+//  attributeBindings: ['iva:value', 'placeholder', 'type'],
+//  type: 'number',
+//  placeholder: null,
+//  iva: (function() {
+//   var res= parseInt(this.get('controller.iva')) * parseInt(this.get('total'));
+//   return isNaN(res)?"":res;
+// 	}).property('controller.iva', 'total')
+// });
 
-App.RisultatoView = Ember.View.extend({
- templateName:"risultato",
- tagName: 'input',
- attributeBindings: ['risultatofinale:value', 'placeholder', 'type'],
- type: 'number',
- placeholder: null,
- risultatofinale: (function() {
- 	var res= this.get('controller.newTransaction.risultatofinale');
- 	return isNaN(res)?"":res;
- 	}).property('controller.newTransaction.total', 'controller.newTransaction.ivamount')
-});
+// App.RisultatoView = Ember.View.extend({
+//  templateName:"risultato",
+//  tagName: 'input',
+//  attributeBindings: ['risultatofinale:value', 'placeholder', 'type'],
+//  type: 'number',
+//  placeholder: null,
+//  risultatofinale: (function() {
+//  	var res= this.get('controller.newTransaction.risultatofinale');
+//  	return isNaN(res)?"":res;
+//  	}).property('controller.newTransaction.total', 'controller.newTransaction.ivamount')
+// });
