e277cc9483bf20908202c7e6255ff56d80f8de3a
Tommaso Villa <tmms.vll@gmail.com>
2015-01-03 20:34:46 +0100

    Iva and Total operation

--- app/assets/javascripts/models/todo.js
+++ app/assets/javascripts/models/todo.js
@@ -4,15 +4,16 @@ App.Fattura = DS.Model.extend({
 });
 
 App.Transaction = DS.Model.extend({
-  quantita      : DS.attr('string'),
-  total         : DS.attr('string'),
-  ivamount      : DS.attr('string'),
-  risulatofinale: DS.attr('string'),
-  fattura       : DS.belongsTo('fattura'),
-  tariffa       : DS.attr('string'),
-  iva           : DS.attr('string'),
-  isChecked     : DS.attr('boolean'),
-  updateTotal: function() {
+  quantita        : DS.attr('string'),
+  tariffa         : DS.attr('string'),
+  lordo           : DS.attr('string'),
+  iva             : DS.attr('string'),
+  ivamount        : DS.attr('string'),
+  netto           : DS.attr('string'),
+  total           : DS.attr('string'),
+  fattura         : DS.belongsTo('fattura'),
+  isChecked       : DS.attr('boolean'),
+  updateLordo: function() {
 
   // get the reference to the values of fare and quantity
   var quantita = this.get('quantita'),
@@ -23,12 +24,44 @@ App.Transaction = DS.Model.extend({
   if (isNaN(quantita)) { quantita = 0; }
 
   // calculate
-  var total = tariffa * quantita;
+  var lordo = tariffa * quantita;
 
-  // set the total
-  this.set('total', total);
+  // set the lordo
+  this.set('lordo', lordo);
 
-}.observes('quantita', 'tariffa')
+}.observes('quantita', 'tariffa'),
+
+  updateIva: function(){
+
+  var iva = this.get('iva'),
+      lordo = this.get('lordo');
+
+  // massage them to make sure your stuff is not gonna break
+  if (isNaN(lordo)) { lordo = 0; }
+  if (isNaN(iva)) { iva = 0; }
+
+  // calculate
+  var ivamount = iva * lordo;
+
+  // set the lordo
+  this.set('ivamount', ivamount);
+
+
+  }.observes('iva', 'lordo'),
+
+  updateRisultato: function(){
+
+  var ivamount = this.get('ivamount'),
+      lordo    = this.get('lordo');
+
+  if (isNaN(ivamount)) {ivamount = 0;}
+  if (isNaN(lordo)) {lordo = 0;}
+
+  var netto = ivamount + lordo
+
+  this.set('netto', netto);
+
+  }.observes('ivamount', 'lordo')
 
 });
 
@@ -47,7 +80,7 @@ App.Transaction.reopenClass({
     {
       id            : '1',
       quantita      : null,
-      total         : null,
+      lordo         : null,
       ivamount      : null,
       risulatofinale: null
     }
