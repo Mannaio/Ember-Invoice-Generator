da3f5359689b8474923af92cf5ad49fbccfab5ba
Tommaso Villa <tmms.vll@gmail.com>
2015-02-08 23:52:51 +0100

    Remove uneccessary code and files

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -2,7 +2,6 @@
 //= require_tree ./models
 //= require_tree ./components
 //= require_tree ./views
-//= require_tree ./localstorage
 //= require ./router
 //= require_self
 //= require_tree ./controllers
